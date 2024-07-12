8dcde0a8f187d0a524965aa0645fe0868ab7f922
Clem0908 <clementlyonnet@hotmail.fr>
2024-07-12 14:25:44 +0200

    First function

---
+++ configure.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python -m venv ./venv
+./venv/bin/pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,32 @@
+import re
+import os
+
+def checkTrackers(email: str):
+   
+    if "track=email" in email:
+        print("\n-> Trackers found !")
+
+    pattern = re.compile("https://[a-zA-Z0-9./?]*track=email",flags=re.DOTALL)
+    res = pattern.findall(email)
+    for i in range(0,len(res)):
+        print("\t- "+str(res[i]))
+
+    return
+
+def checkSpyingPixel(email: str):
+    
+    
+
+    return
+
+if __name__ == "__main__":
+
+    if os.path.exists("./email.eml") is False:
+        print("Please insert all the content of your email into './email.eml'")
+        exit()
+
+    f = open("./email.eml","r")
+    email = f.read()
+    f.close()
+    checkTrackers(email)
+    checkSpyingPixel(email)
