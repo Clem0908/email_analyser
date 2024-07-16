5c9316881b47545a9020ee2c80020a3f30296919
Clem0908 <clementlyonnet@hotmail.fr>
2024-07-16 23:11:40 +0200

    New rule for spy

--- main.py
+++ main.py
@@ -22,6 +22,14 @@ def checkSpyingPixel(email: str):
         res = pattern.findall(email)
         for i in range(0,len(res)):
             print("\t- "+str(res[i]))
+    
+    if "width='1' height='1'" in email:
+        
+        print("\n-> Spying pixel found !")
+        pattern = re.compile("<img [a-zA-Z0-9./:?=\"_]* width='1' height='1'>",flags=re.DOTALL)
+        res = pattern.findall(email)
+        for i in range(0,len(res)):
+            print("\t- "+str(res[i]))
 
     return
 
