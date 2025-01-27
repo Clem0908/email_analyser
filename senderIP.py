c249ec74f89510fea72bea051ad6b69f4c8a68fc
Clem0908 <clementlyonnet@hotmail.fr>
2025-01-27 19:16:05 +0100

    better regex for IPs

--- senderIP.py
+++ senderIP.py
@@ -2,13 +2,17 @@ import re
 
 def senderIP(email: str):
 
-    res = "<p><b>Sender IP is:</b> "
-    
-    pattern = re.compile("sender IP is [0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}",flags=re.DOTALL)
+    pattern = re.compile("[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}",flags=re.DOTALL)
     find = pattern.findall(email)
-    if len(find) != 0:
-        find[0] = find[0].replace("sender IP is ", "")
-        res += find[0]
 
-    res += "</p>"
+    if len(find) > 0:
+
+        res = "<table border=solid><b>Detected IPs: </b><br></br><tr>"
+
+        for i in range(0, len(find)):
+
+            res += "<td>" + find[i] + "</td>"
+    
+        res += "</tr></table>"
+
     return res
