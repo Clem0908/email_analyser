162f3e47bc033622e5202f8a556edd0ee4311299
Clem0908 <clementlyonnet@hotmail.fr>
2025-01-31 11:15:10 +0100

    fix escape sequence

--- checkMailHeaders.py
+++ checkMailHeaders.py
@@ -3,19 +3,19 @@ import re
 def checkMailHeaders(email: str):
 
     res = ""
-    pattern = re.compile("spf=\w+",flags=re.DOTALL)
+    pattern = re.compile("spf=\\w+",flags=re.DOTALL)
     find = pattern.findall(email)
     if len(find) > 0:
         spf = find[0].replace("spf=", "")
         res += "<p><b>SPF: </b>"+spf+"</p>"
 
-    pattern = re.compile("dkim=\w+",flags=re.DOTALL)
+    pattern = re.compile("dkim=\\w+",flags=re.DOTALL)
     find = pattern.findall(email)
     if len(find) > 0:
         dkim = find[0].replace("dkim=", "")
         res += "<p><b>DKIM: </b>"+dkim+"</p>"
 
-    pattern = re.compile("dmarc=\w+",flags=re.DOTALL)
+    pattern = re.compile("dmarc=\\w+",flags=re.DOTALL)
     find = pattern.findall(email)
     if len(find) > 0:
         dmarc = find[0].replace("dmarc=", "")
