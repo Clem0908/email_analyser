80e764f622299d4f4d476422e658f0245ec3ad3b
Clem0908 <clementlyonnet@hotmail.fr>
2025-03-18 23:12:45 +0100

    regex upgrade

--- checkSpyingPixel.py
+++ checkSpyingPixel.py
@@ -2,7 +2,7 @@ import re
 
 def checkSpyingPixel(email: str):
     
-    CASES = "(width=[0-1]{1})|(width=\"[0-1]\"{1})|(width='[0-1]'{1})|(width=3D\"[0-1]\"{1})|(width=\"[0-1]{1}px\")|(width:[0-1]{1}px)|(width: [0-1]{1}px)|(width:[0-1]{1})"
+    CASES = "(width=[0-1]{1})|(width=\"[0-1]\"{1})|(width='[0-1]'{1})|(width=3D\"[0-1]\"{1})|(width=\"[0-1]{1}px\")|(width:[0-1]{1}px)|(width: [0-1]{1}px)|(width:[0-1]{1})|(width=3D=\'[0-1]{1}\')"
 
     res = ""
     pattern = re.compile(CASES, flags = re.DOTALL)
@@ -10,7 +10,7 @@ def checkSpyingPixel(email: str):
     
     if len(find) > 0:
         res += "<h2><b>Spying pixels found</b></h2>"
-        pattern = re.compile("<img [a-zA-Z=\"'\\/%0-9-_.:,;&?!#~{}\n+\\[\\] ]*>", flags=re.DOTALL)
+        pattern = re.compile("<img [^>]*", flags=re.DOTALL)
         find = pattern.findall(email)
         res += "<p><b>Images found:</b></p><table border=solid cellpadding=\"15%\">"
         for i in range(0, len(find)):
